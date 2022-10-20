200a6f82b930cc4af0bc5b4ba0435fe7216e17e3
Nícolas <nicolasbrga01@gmail.com>
2022-10-20 20:35:13 -0300

    consertei o lint

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -38,16 +38,16 @@ const calculator = (number1, number2) => {
   const calc = {
     sum: number1 + number2,
     mult: number1 * number2,
-    div: Math.floor(number1/number2),
+    div: Math.floor(number1 / number2),
     sub: number1 - number2,
-  }
+  };
   return calc;
 };
 
-const arrayGenerator = (type, object) => {
- if (type === 'keys'){
+//  if (type === 'keys') {
   
- }
+//  }
+const arrayGenerator = (type, object) => {
 };
 
 module.exports = { calculator, arrayGenerator };
